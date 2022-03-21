0f366d32ef42b1aeec4317954ee86781e0262b5c
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-03-21 12:13:01 +0000

    creating the graphql schema based on our prisma schema

--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,27 @@ export const typeDefs = gql`
   type Query {
     hello: String!
   }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    profile: Profile!
+    posts: [Post!]!
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    user: User!
+  }
 `
