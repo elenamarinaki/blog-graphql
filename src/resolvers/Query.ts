49441398ac7c1a608f2a076beee0e01a93f3bc1b
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-04-12 00:26:16 +0100

    added me query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,14 @@
 import { Context } from "../index"
 
 export const Query = {
+  me: (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null
+    return prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    })
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     return await prisma.post.findMany({
       orderBy: [{ createdAt: "desc" }],
