6d6641da1e5848e8bb5a800483ebe4315745f0fe
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-05-12 18:08:30 +0100

    creating the first posts query in the FE

--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -1,6 +1,20 @@
-import React from "react";
-import Post from "../../components/Post/Post";
+import React from "react"
+import Post from "../../components/Post/Post"
+import { gql } from "@apollo/client"
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      title
+      content
+      createdAt
+      user {
+        name
+      }
+    }
+  }
+`
 
 export default function Posts() {
-  return <div></div>;
+  return <div></div>
 }
